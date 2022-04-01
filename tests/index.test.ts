b3a322df6b54186d96589bd856de449531862eef
Bauke <me@bauke.xyz>
2022-04-01 12:24:18 +0200

    Rewrite the tests in TypeScript.

---
+++ tests/index.test.ts
@@ -0,0 +1,17 @@
+import test from 'ava';
+
+import owofy, {faces} from '../source/index.js';
+
+test('Owofy', (t) => {
+  t.is(typeof owofy, 'function');
+
+  const owofied = owofy('Lorem ipsum dolor sit amet.');
+  t.snapshot(owofied, 'Lorem ipsum example');
+});
+
+test('Faces', (t) => {
+  const owofied = owofy('Hey!!');
+  t.true(faces.length > 0);
+  t.true(faces.some((face) => owofied.includes(face)));
+  t.true(owofied.includes('! '));
+});
